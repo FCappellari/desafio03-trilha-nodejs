5d816f9ef91f7b815e48c28a83a75ff56ff40598
Fabrício Cappellari <fabricio.cappellari@live.com>
2021-12-09 00:34:23 -0300

    third challenge rocketseat

--- src/index.js
+++ src/index.js
@@ -23,24 +23,29 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const updatedRepository = { ...repositories[repositoryIndex] };
+  updatedRepository.title = title;
+  updatedRepository.url   = url;
+  updatedRepository.techs = techs;
 
-  repositories[repositoryIndex] = repository;
+  repositories[repositoryIndex] = updatedRepository;
 
-  return response.json(repository);
+  return response.json(updatedRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -48,7 +53,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -67,8 +72,9 @@ app.post("/repositories/:id/like", (request, response) => {
   }
 
   const likes = ++repositories[repositoryIndex].likes;
+  repositories[repositoryIndex].likes = likes;
 
-  return response.json('likes');
+  return response.json({ likes: likes });
 });
 
 module.exports = app;
